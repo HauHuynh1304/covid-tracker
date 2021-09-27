e084b3d8304d3695e3e4dec1e6205683a2770c7a
“HauHuynh1304” <hauhuynh1304@gmail.com>
2021-09-27 11:20:36 -0700

    build header

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>app</h1>
+      <Header></Header>
     </div>
   );
 }
